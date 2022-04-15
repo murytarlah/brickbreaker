42167783642e687bba45f82ebffc0288fe6fd8f5
murytarlah <dknight.jim@gmail.com>
2022-04-15 17:01:01 +0100

    implemented brick collision

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ let bricks = [];
 for (let i = 0; i < brickColumnCount; i++) {
     bricks[i] = [];
     for (let r = 0; r < brickRowCount; r++) {
-        bricks[i][r] = { x: 0, y: 0 };
+        bricks[i][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
@@ -61,18 +61,35 @@ function keyUpHandler(e) {
 function drawBricks() {
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
-            let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-            let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+            if (bricks[c][r].status == 1) {
+                let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
+
+function collisionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            let b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
 
 
 
@@ -100,6 +117,8 @@ function draw() {
     drawBricks()
     drawBall();
     drawPaddle();
+    collisionDetection();
+
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
@@ -111,10 +130,11 @@ function draw() {
         dy = -dy;
     } else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
-            dy = -dy;
+            if (y = y - paddleHeight) {
+                dy = -dy;
+            }
         }
         else {
-            // Needed for Chrome to end game
             alert("GAME OVER");
             document.location.reload();
             clearInterval(interval);
@@ -122,17 +142,11 @@ function draw() {
     }
 
 
-    if (rightPressed) {
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width) {
-            paddleX = canvas.width - paddleWidth;
-        }
     }
-    else if (leftPressed) {
+    else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
-        if (paddleX < 0) {
-            paddleX = 0;
-        }
     }
 
     x += dx;
